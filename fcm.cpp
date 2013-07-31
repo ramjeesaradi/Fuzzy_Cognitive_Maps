b55a244b5b2548bd1ea03561f08c66b81ccc10c1
Samuel Chase <samebchase@gmail.com>
2013-07-31 14:34:48 +0530

    Remove Debug/* and make project to build again

--- fcm.cpp
+++ fcm.cpp
@@ -150,16 +150,15 @@ array<double, 100> pso(double epsilon, double omega, double phyp, double phyg){
 }
 
 int main() {
-   ofstream otwts("weights.csv")
+    ofstream otwts("weights.csv");
 	//fitness();
-   double temp[]= pso(0.2,0.5,0.5,0.5);
-   for(int m = 0; m < 10; m++)    { //import the weights
-           for(int n = 0; n < 10; n++)        {
-              otwts << temp[m*10 + n] << "\t";
-               //cout <<m*10 + n <<" " << dmns[m*10 + n] << endl;
-               //cout << w[m][n] << endl ;
-           }otwts << endl;
-       }
+    array<double, 100> = pso(0.2,0.5,0.5,0.5);
+    for(int m = 0; m < 10; m++)    { //import the weights
+        for(int n = 0; n < 10; n++)        {
+            otwts << temp[m*10 + n] << "\t";
+            //cout <<m*10 + n <<" " << dmns[m*10 + n] << endl;
+            //cout << w[m][n] << endl ;
+        } otwts << endl;
+    }
     return 0;
-
 }
